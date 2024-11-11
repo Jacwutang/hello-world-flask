aef8cd9e55250b7c133a6aa1ee3ed2aa9da5162f
admin <jack@Jacks-MacBook-Pro.local>
2024-11-11 13:10:28 -0800

    add ssl

--- app/run.py
+++ app/run.py
@@ -11,4 +11,4 @@ with app.app_context():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(ssl_context='adhoc', host='0.0.0.0', port=5001, debug=True)
